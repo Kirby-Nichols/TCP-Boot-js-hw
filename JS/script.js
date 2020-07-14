b07c6550a1b47124c739cb4fcb10716b7a040b77
Kirby Nichols <kirby.nichols9@outlook.com>
2020-07-14 18:32:42 -0400

    update hmwk

---
+++ JS/script.js
@@ -0,0 +1,25 @@
+$(".changeBtn1").click(function(){
+
+    $(".firstDiv").addClass("firstDivnew");
+})
+
+
+$(".changeBtn2").click(function(){
+
+})
+
+$(".changeBtn3").click(function(){
+
+})
+
+$(".changeAllBtn").click(function(){
+
+    $(".firstDiv").addclass("changeAllColors");
+    $(".secondDiv").addclass("firstDivnew");
+    $(".thirdDiv").addclass("thirdDivnew");
+
+})
+
+$(".hidebtn1").hide(function(){
+    
+})
